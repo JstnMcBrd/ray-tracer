fe28abee42006a56db409fea643b01cd1324f80e
Justin McBride <jstnmcbrd@outlook.com>
2023-02-23 12:22:37 -0700

    Add basic raytracing

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import argparse
 from dotenv import load_dotenv
 import json
+from math import tan
 import numpy as np
 from os import getenv
 
 from SceneImporter import loadFromJson
-from Scene import Scene
+from Scene import Scene, Sphere
 
 
 def loadScene(filePath: str) -> Scene:
@@ -32,21 +33,82 @@ def loadScene(filePath: str) -> Scene:
 
 	return scene
 
+
 def writeToPPM(screen: np.ndarray, outputFilePath: str):
 	outputFile = open(outputFilePath, "w")
 	outputFile.write("P3\n")
-	outputFile.write(f"{len(screen[0])} {len(screen)}\n")
-	outputFile.write("255\n")
-	for row in screen:
-		for pixel in row:
-			outputFile.write(f"{pixel[0]} {pixel[1]} {pixel[2]} ")
+	outputFile.write(f"{len(screen)} {len(screen[0])}\n")
+	outputFile.write("1\n")
+	for y in range(len(screen[0])):
+		for x in range(len(screen)):
+			pixel = screen[x,y]
+			outputFile.write(f"{pixel[0]} {pixel[1]} {pixel[2]}     ")
 		outputFile.write("\n")
 
 
 def raytrace(scene: Scene, width: int, height: int) -> np.ndarray:
-	screen = np.zeros((height, width, 3))
+	screen = np.zeros((width, height, 3))
+	viewportSize = np.array([width, height])
+	windowSize = calculateWindowSize(viewportSize, scene.cameraLookAt, scene.cameraLookFrom, scene.fieldOfView)
+
+	for x in range(len(screen)):
+		for y in range(len(screen[x])):
+			viewportPoint = np.array([x, y])
+			windowPoint = viewportToWindow(viewportPoint, viewportSize, windowSize)
+			worldPoint = windowToWorld(windowPoint, scene.cameraForward(), scene.cameraUp(), scene.cameraRight())
+
+			rayOrigin = scene.cameraLookFrom
+			rayDirection = worldPoint - scene.cameraLookFrom
+			rayDirection /= np.sqrt(np.dot(rayDirection,rayDirection)) # normalize
+
+			for obj in scene.objects:
+				if type(obj) is Sphere:
+					intersection = raySphereIntersection(rayOrigin, rayDirection, obj)
+					if not type(intersection) is type(None):
+						screen[x,y] = obj.color()
 
 	return screen
+
+
+def calculateWindowSize(viewportSize: np.ndarray, cameraLookAt: np.ndarray, cameraLookFrom: np.ndarray, fieldOfView: float) -> np.ndarray:
+	dist = cameraLookAt - cameraLookFrom
+	dist_mag = np.sqrt(np.dot(dist,dist))
+	x = dist_mag * tan(np.deg2rad(fieldOfView/2)) * 2
+	y = x * viewportSize[1]/viewportSize[0]
+	return np.array([x, y])
+
+
+def viewportToWindow(viewportPoint: np.ndarray, viewportSize: np.ndarray, windowSize: np.ndarray) -> np.ndarray:
+	windowPoint = viewportPoint * windowSize/viewportSize - windowSize/2
+	return np.array([windowPoint[0], windowPoint[1]*-1, 0]) # The -1 seems necessary to orient it correctly
+
+
+def windowToWorld(windowPoint: np.ndarray, cameraForward: np.ndarray, cameraUp: np.ndarray, cameraRight: np.ndarray) -> np.ndarray:
+	return scene.cameraLookAt + windowPoint[0]*cameraRight + windowPoint[1]*cameraUp + windowPoint[2]*cameraForward
+
+
+def raySphereIntersection(rayOrigin: np.ndarray, rayDirection: np.ndarray, sphere: Sphere):
+	dist = sphere.center - rayOrigin
+	dist_sqr = np.dot(dist, dist)
+	dist_mag = np.sqrt(dist_sqr)
+
+	outside = dist_mag >= sphere.radius
+
+	closestApproach = np.dot(rayDirection, dist)
+
+	if closestApproach < 0 and outside:
+		return None
+
+	closestApproachDistToSurface_sqr = sphere.radius**2 - dist_sqr + closestApproach**2
+
+	if closestApproachDistToSurface_sqr < 0:
+		return None
+	
+	closestApproachDistToSurface = closestApproachDistToSurface_sqr**0.5
+
+	t = closestApproach - closestApproachDistToSurface if outside else closestApproach + closestApproachDistToSurface
+
+	return rayOrigin + rayDirection*t
 
 
 if __name__ == "__main__":
