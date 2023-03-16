7523129a3a1fef8a016eeb07dc8c13fe9ce7f4cb
Justin McBride <jstnmcbrd@outlook.com>
2023-03-15 20:16:31 -0600

    Lint vector_utils.py

--- src/vector_utils.py
+++ src/vector_utils.py
@@ -1,11 +1,17 @@
+"""
+Contains useful methods for manipulating numpy arrays as vectors.
+"""
+
+
 import numpy as np
 
-# In this project, vectors are represented with 1x3 numpy arrays
 
 def magnitude(vector: np.ndarray) -> float:
+	""" Returns the scalar length of the vector. """
 	return np.sqrt(np.dot(vector, vector))
 
 
 def normalized(vector: np.ndarray) -> np.ndarray:
+	""" Returns a new vector pointing in the same direction but with a length of 1 (or 0). """
 	mag = magnitude(vector)
 	return vector / mag if mag != 0 else vector
