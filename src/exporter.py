cb05e4eff28ec65185735dc39146c054ca66a4ce
Justin McBride <jstnmcbrd@outlook.com>
2024-03-02 03:48:48 -0700

    Prevent assertion from deleting old output

--- src/exporter.py
+++ src/exporter.py
@@ -4,20 +4,32 @@ Contains methods for writing the screen to image files.
 
 
 from os import remove
+from random import choice
 import sys
+from string import ascii_letters
 
 import numpy as np
 from PIL import Image
 
 
+COLOR_MODE = 'RGB'
+
+
+def random_str(length):
+	""" Generates a random string of the given length. """
+
+	return ''.join(choice(ascii_letters) for _ in range(length))
+
+
 def assert_supported_extension(output_file_path: str) -> None:
 	""" Checks whether the file path with the given extension is supported by Pillow. """
 
+	extension = output_file_path.split(".")[-1]
+	random_file_name = f"temp_{random_str(10)}.{extension}"
 	try:
-		Image.new('RGB', (1, 1)).save(output_file_path)
-		remove(output_file_path)
+		Image.new(COLOR_MODE, (1, 1)).save(random_file_name)
+		remove(random_file_name)
 	except ValueError:
-		extension = output_file_path.split(".")[-1]
 		print(f"Output file extension is not supported: {extension}")
 		sys.exit(1)
 
@@ -37,6 +49,6 @@ def export(screen: np.ndarray, output_file_path: str) -> None:
 	screen.resize((width * height, 3))
 	data: list = [tuple(pixel) for pixel in screen]
 
-	image = Image.new('RGB', (width, height))
+	image = Image.new(COLOR_MODE, (width, height))
 	image.putdata(data)
 	image.save(output_file_path)
