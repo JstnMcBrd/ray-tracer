Fix screen indexing to fix rotation issue

src/exporter.py
@@ -35,16 +35,10 @@ def assert_supported_extension(output_file_path: str) -> None:
 def export(screen: np.ndarray, output_file_path: str) -> None:
 	"Writes the screen to a file using the encoding of the file extension."
 
-	width = len(screen)
-	height = len(screen[0])
+	height, width, depth = screen.shape
 
 	screen = (screen * 255).astype(np.uint8)
-
-	# pylint: disable-next=fixme
-	# FIXME making up for the screen being indexed by [col, row] instead of [row, col]
-	screen = np.array(Image.fromarray(screen).rotate(-90))
-
-	screen.resize((width * height, 3))
+	screen.resize(width * height, depth)
 	data: list = [tuple(pixel) for pixel in screen]
 
 	image = Image.new(COLOR_MODE, (width, height))

src/ray_tracer.py
@@ -31,7 +31,7 @@ def ray_trace(scene: Scene, width: int, height: int,
 
 	# Set up multiprocessing pool and inputs
 	tuple_inputs = [(x, y, scene, window_to_viewport_size_ratio, half_window_size, reflection_limit)
-		for x in range(width) for y in range(height)]
+		for y in range(height) for x in range(width)]
 	outputs = []
 
 	with Pool(cpu_count()) as pool:
@@ -45,7 +45,9 @@ def ray_trace(scene: Scene, width: int, height: int,
 		outputs = list(processes)
 
 	# Shape outputs into a width*height screen
-	screen = np.array(outputs).reshape((width, height, 3))
+	screen = np.array(outputs)
+	_, depth = screen.shape
+	screen.resize((height, width, depth))
 
 	return screen
 